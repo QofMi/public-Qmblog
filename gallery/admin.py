096e59b0f0583eb88650b16df6f79bc75b559f10
QofMi <avatar11132@gmail.com>
2021-01-05 15:01:04 +0700

    Added new model - LoadMediaImage

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Gallery
+from .models import Gallery, LoadMediaImage
 from django.utils.html import mark_safe
 
 
@@ -7,6 +7,20 @@ from django.utils.html import mark_safe
 class AdminGallery(admin.ModelAdmin):
     """
     Регистраци модели Gallery в административной части сайта
+    """
+    list_display = ['title', 'date_pub', 'get_img']
+    exclude = ['slug']
+    readonly_fields = ('get_img', 'date_pub',)
+    def get_img(self, obj):
+        return mark_safe(f'<img src={obj.img.url} width="200" height="auto">')
+
+    get_img.short_description = 'Изображение'
+
+
+@admin.register(LoadMediaImage)
+class AdminLoadMediaImage(admin.ModelAdmin):
+    """
+    Регистраци модели LoadMediaImage в административной части сайта
     """
     list_display = ['title', 'date_pub', 'get_img']
     exclude = ['slug']
--- gallery/models.py
+++ gallery/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from blog.images import _compressing_image, upload_to_gallery
+from blog.images import GenFilename
 from blog.utils import _gen_slug
 
 
@@ -9,13 +9,36 @@ class Gallery(models.Model):
     """
     title = models.CharField('Заголовок', max_length=150, db_index=True)
     slug = models.SlugField('Идентификатор',max_length=150, blank=True, unique=True)
-    img = models.ImageField('Изображение',upload_to=upload_to_gallery,)
+    img = models.ImageField('Изображение',upload_to=GenFilename('gallery'),)
     date_pub = models.DateTimeField('Дата публикации', auto_now_add=True)
 
     class Meta:
          ordering = ['-date_pub',]
          verbose_name = 'Галерея'
          verbose_name_plural = "Галерея"
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = _gen_slug(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class LoadMediaImage(models.Model):
+    """
+    Модель изображений загруженных в медиа
+    """
+    title = models.CharField('Заголовок', max_length=150, db_index=True)
+    slug = models.SlugField('Идентификатор',max_length=150, blank=True, unique=True)
+    img = models.ImageField('Изображение',upload_to=GenFilename('dc1a3df56a26bb44dcf26'),)
+    date_pub = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    class Meta:
+         ordering = ['-date_pub',]
+         verbose_name = 'Загруженные изображения'
+         verbose_name_plural = "Загруженные изображения"
 
     def save(self, *args, **kwargs):
         if not self.id:
